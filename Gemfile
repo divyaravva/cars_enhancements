e7db860c4002914b2adec31eae6da99f06fb7149
Divya <divyaravva@gmail.com>
2018-09-19 20:53:29 -0500

    weather api

--- Gemfile
+++ Gemfile
@@ -13,7 +13,7 @@ gem 'wkhtmltopdf-binary'
 
 gem 'jquery-tablesorter'
 gem 'bootstrap-sass', '~> 3.2.0'
-
+gem 'open-weather'
 
 gem 'jquery-rails'
 
--- app/models/car.rb
+++ app/models/car.rb
@@ -1,4 +1,12 @@
 class Car < ApplicationRecord
-
+	
+	require 'open_weather'
+	WEATHER_API_KEY= "9412c4a3513302a0699db4659eae553e"
 	has_many :comments, :dependent => :destroy, :foreign_key => "car_id"
+
+
+	def self.show_weather_based_on_city
+		options = { units: "metric", APPID: WEATHER_API_KEY }
+		OpenWeather::Current.city_id("5219003", options)
+	end
 end
